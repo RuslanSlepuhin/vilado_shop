523ccdd3582afeba3db71bc5788088ab557e5891
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-08-20 21:34:57 +0300

    parser

--- service_apps/chrome_settings.py
+++ service_apps/chrome_settings.py
@@ -5,7 +5,8 @@ chrome_driver_path = "./service_apps/chrome_driver/chromedriver.exe" if os_syste
 ubuntu_chrome_driver_path = "./service_apps/chrome_driver/chromedriver"
 
 options = Options()
-# options.add_argument('--headless')
+if os_system == 'ubuntu':
+    options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
